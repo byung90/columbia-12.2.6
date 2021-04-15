d3a3622ffe1270d434c93a2b643748ec26725762
Byungwook Yoon <byung90@gmail.com>
2021-04-15 19:36:57 -0400

    complete

---
+++ playlistSeeds.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS playlistDB;
+
+CREATE DATABASE playlistDB;
+
+USE playlistDB;
+
+CREATE TABLE songs (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(45) NULL,
+  artist VARCHAR(45) NULL,
+  genre VARCHAR(45) NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO songs (title, artist, genre)
+VALUES ("Yellow submarine", "Beetles", "Rock");
+
+
+-- ### Alternative way to insert more than one row
+-- INSERT INTO products (flavor, price, quantity)
+-- VALUES ("vanilla", 2.50, 100), ("chocolate", 3.10, 120), ("strawberry", 3.25, 75);
